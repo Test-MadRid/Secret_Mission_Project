d925f1702e91a877e529f0a941377445342ab426
Test-MadRid <144245828+Test-MadRid@users.noreply.github.com>
2023-11-27 15:49:51 +0700

    Project Secret Mission
    Pun 2 Game Play

---
+++ Scripts/ScoreboardItem.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using Photon.Realtime;
+using System;
+using Photon.Pun;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+public class ScoreboardItem : MonoBehaviourPunCallbacks
+{
+    public TMP_Text UsernameText;
+    public TMP_Text KillsText;
+    public TMP_Text DeathsText;
+
+    Player player;
+    public void initialize(Player player)
+    {
+        this.player = player; 
+
+        UsernameText.text = player.NickName;
+        UpdateStats();
+    }
+    void UpdateStats()
+    {
+        if (player.CustomProperties.TryGetValue("kills", out object kills))
+        {
+            KillsText.text = kills.ToString();
+        }
+        if (player.CustomProperties.TryGetValue("deaths", out object deaths))
+        {
+            DeathsText.text = deaths.ToString();
+        }
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if (targetPlayer == player)
+        {
+            if (changedProps.ContainsKey("kills") || changedProps.ContainsKey("deaths"))
+            {
+                UpdateStats();
+            }
+        }
+    }
+
+}
---
+++ Scripts/SingleShotgun.cs
@@ -0,0 +1,44 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingleShotgun : Gun
+{
+    [SerializeField] Camera cam;
+
+    PhotonView PV;
+
+    private void Awake()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+    public override void Use()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3 (0.5f, 0.5f));
+        ray.origin = cam.transform.position;
+        if(Physics.Raycast(ray, out RaycastHit hit))
+        {
+            hit.collider.gameObject.GetComponent < IDamageable>()?.TakeDamage(((GunInfo)itemInfo).damage);
+
+            PV.RPC("RPC_Shoot", RpcTarget.All, hit.point, hit.normal);
+        }
+    }
+
+    [PunRPC]
+    void RPC_Shoot(Vector3 hitPosition, Vector3 hitNormal)
+    {
+        Collider[] collider = Physics.OverlapSphere(hitPosition, 0.3f);
+        if (collider.Length != 0)
+        {
+            GameObject bulletImpactObj = Instantiate(bulletImpactPrefab, hitPosition + hitNormal * 0.001f, Quaternion.LookRotation(hitNormal, Vector3.up) * bulletImpactPrefab.transform.rotation);
+            Destroy(bulletImpactObj, 10f);
+            bulletImpactObj.transform.SetParent(collider[0].transform);
+        }
+    }
+}
